be2479367029769327a9eb184f9a51457f031c14
ourabah <ourabah.lou@hotmail.com>
2023-10-09 12:04:09 +0200

    Create Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom'; // Importer NavLink si vous utilisez React Router
+import 'font-awesome/css/font-awesome.min.css'; // Importer FontAwesome si vous utilisez les icônes FontAwesome
+import '../style/Header.css'; // Importer le CSS de votre NavBar si vous l'avez externalisé
+
+
+
+
+function NavBar() {
+    const [click, setClick] = React.useState(false);
+  
+    const handleClick = () => setClick(!click);
+    const Close = () => setClick(false);
+    
+    return (
+      <div>
+       <div className={click ? "main-container" : ""}  onClick={()=>Close()} />
+        <nav className="navbar" onClick={e => e.stopPropagation()}>
+          <div className="nav-container">
+            <NavLink exact to="/" className="nav-logo">
+              CarApp
+              <i className="fa fa-code"></i>
+            </NavLink>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/"
+                  activeClassName="active"
+                  className="nav-links"
+                  onClick={click ? handleClick : null}
+                >
+                  Home
+                </NavLink>
+              </li>
+                
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/Gallery"
+                  activeClassName="active"
+                  className="nav-links"
+                  onClick={click ? handleClick : null}
+                >
+                  Gallery
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/about"
+                  activeClassName="active"
+                  className="nav-links"
+                  onClick={click ? handleClick : null}
+                >
+                  About
+                </NavLink>
+              </li>
+
+              
+              <li className="nav-item">
+                <NavLink
+                  exact
+                  to="/contact"
+                  activeClassName="active"
+                  className="nav-links"
+                 onClick={click ? handleClick : null}
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+            <div className="nav-icon" onClick={handleClick}>
+              <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
+            </div>
+          </div>
+        </nav>
+      </ div>
+    );
+  }
+export default NavBar;
