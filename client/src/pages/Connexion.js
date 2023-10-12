24763f9187537221eef21f22dacd3916ee4f38fb
ourabah <ourabah.lou@hotmail.com>
2023-10-12 15:27:50 +0200

    up

--- client/src/pages/Connexion.js
+++ client/src/pages/Connexion.js
@@ -1,11 +1,16 @@
-import { useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import '../style/connexion.css';
 import { AuthContext } from '../context/AuthContext.js';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
-export const Connexion = () => {
+const usernameRegex = /^[a-zA-Z0-9_]{3,20}$/;
 
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z0-9]{2,}$/i;
+const passwordRegex = /^[a-zA-Z0-9]{4,}$/;
+
+
+export const Connexion = () => {
     const navigate = useNavigate();
     const { login } = useContext(AuthContext);
     const [activeTab, setActiveTab] = useState('register'); // Onglet actif par défaut
@@ -15,44 +20,60 @@ export const Connexion = () => {
         password: ""
     });
 
+    const [connectError, setConnectError] = useState('');
+
     const changeConnect = (e) => {
-        setInputsConnect(prev => ({ ...prev, [e.target.name]: e.target.value }))
-    }
+        setConnectError('');
+        setInputsConnect((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
 
     const handleConnect = async (e) => {
         e.preventDefault();
-        try {
-            await login(inputsConnect)
-            console.log("yay t'es connecté")
-            navigate("/admin/gestion")
-        }
-        catch (err) {
-            console.log('bah ça se connecte pas oupsi + ' + err);
-        }
-    }
 
-    // PARTIE QUI GERE L'INSCRIPTION
+        if (!usernameRegex.test(inputsConnect.username) || !passwordRegex.test(inputsConnect.password)) {
+            setConnectError('Nom d\'utilisateur ou mot de passe invalide.');
+            return;
+        }
+
+        try {
+            await login(inputsConnect);
+            console.log("Vous êtes connecté !");
+            navigate("/admin/gestion");
+        } catch (err) {
+            setConnectError("Une erreur s'est produite lors de la connexion : " + err);
+        }
+    };
+
+    // PARTIE QUI GÈRE L'INSCRIPTION
     const [inputsRegister, setInputsRegister] = useState({
         username: "",
         email: "",
         password: ""
-    })
+    });
 
-    const handleRegister = async (e) => {
-        e.preventDefault()
-        try {
-            const res = await axios.post("/enregistrer", inputsRegister)
-            console.log(res)
-            window.location.reload()
-        }
-        catch (err) {
-            console.log('handle register err: ' + err)
-        }
-    }
+    const [registerError, setRegisterError] = useState('');
 
     const changeRegister = (e) => {
-        setInputsRegister(prev => ({ ...prev, [e.target.name]: e.target.value }))
-    }
+        setRegisterError('');
+        setInputsRegister((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+
+        if (!usernameRegex.test(inputsRegister.username) || !emailRegex.test(inputsRegister.email) || !passwordRegex.test(inputsRegister.password)) {
+            setRegisterError('Veuillez remplir correctement tous les champs.');
+            return;
+        }
+
+        try {
+            const res = await axios.post("/enregistrer", inputsRegister);
+            console.log(res);
+            window.location.reload();
+        } catch (err) {
+            setRegisterError("Une erreur s'est produite lors de l'inscription : " + err);
+        }
+    };
 
     return (
         <div className='log'>
@@ -80,6 +101,7 @@ export const Connexion = () => {
                         <input placeholder="email" name="email" type="email" onChange={changeRegister} />
                         <label>PASSWORD</label>
                         <input placeholder="password" name="password" type="password" onChange={changeRegister} />
+                        {registerError && <div className="error-message">{registerError}</div>}
                         <button className="enregistrer-button" onClick={handleRegister}>REGISTER</button>
                     </form>
                 </div>
@@ -88,13 +110,13 @@ export const Connexion = () => {
                     <form className="connecter-form">
                         <label>USERNAME</label>
                         <input placeholder="username" name="username" type="text" onChange={changeConnect} />
-                        <label>Password</label>
+                        <label>PASSWORD</label>
                         <input placeholder="password" name="password" type="password" onChange={changeConnect} />
+                        {connectError && <div className="error-message">{connectError}</div>}
                         <button className="connecter-button" onClick={handleConnect}>CONNECT</button>
                     </form>
                 </div>
             </div>
         </div>
-    )
-}
-
+    );
+};
